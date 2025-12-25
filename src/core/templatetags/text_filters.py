d29552c084487c69d42502763ad32e46e096159e
Dario <dario@mail.com>
2025-12-25 23:41:47 +0100

    Remove unused stuff

--- src/core/templatetags/text_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-from src.core.utils import full_url_for_path
-
-register = template.Library()
-
-@register.filter
-def full_url_filter(value):
-    return full_url_for_path(value)
--- src/payment/value_objects/payout_value_object.py
+++
@@ -1,4 +0,0 @@
-class PayoutValueObject:
-    def __init__(self, provider: str, result: bool):
-        self.provider = provider
-        self.result = result
