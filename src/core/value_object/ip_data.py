c62b47cf72e4d6a52792c69eb0f0ceb157020aad
Dario <dario@mail.com>
2025-12-29 21:42:07 +0100

    Remove unused stuff

--- src/core/value_object/ip_data.py
+++
@@ -1,8 +0,0 @@
-class IpData:
-    def __init__(self, timezone=None, country_code=None, state_code=None):
-        self.timezone = timezone
-        self.country_code = country_code
-        self.state_code = state_code
-
-    def is_usa(self) -> bool:
-        return self.country_code == 'US'
--- src/notification/services/web_push_notifications/web_push_subscribe_service.py
+++
@@ -1,6 +0,0 @@
-from src.user.models import User
-
-
-class WebPushSubscribeService:
-    def push_subscribe(self, user: User, subscription: dict) -> None:
-        WebPushSubscription.objects.create(user=user, subscription=subscription)
