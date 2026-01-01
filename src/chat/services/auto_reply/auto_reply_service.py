133352471ef18d857dfa5638e84462d420911834
Dario <dario@mail.com>
2026-01-01 22:06:21 +0100

    use async/await

--- src/chat/services/auto_reply/auto_reply_service.py
+++ src/chat/services/auto_reply/auto_reply_service.py
@@ -4,6 +4,6 @@ from chatapp import settings
 
 
 class AutoReplyService:
-    def reply_now(self, message: str, chat_id: int) -> None:
+    async def reply_now(self, message: str, chat_id: int) -> None:
         bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
-        bot.send_message(chat_id=chat_id, text=message)
+        await bot.send_message(chat_id=chat_id, text=message)
