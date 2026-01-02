5d71a20cab7f5cde59d510c4418d42ed1c43aa8f
Dario <dario@mail.com>
2026-01-02 12:43:54 +0100

    use asyncio to run function

--- src/chat/services/auto_reply/auto_reply_service.py
+++ src/chat/services/auto_reply/auto_reply_service.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import bugsnag
 from telegram import Bot
 
@@ -8,6 +10,10 @@ class AutoReplyService:
     def reply_now(self, message: str, chat_id: int) -> None:
         try:
             bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
-            bot.send_message(chat_id=chat_id, text=message)
+
+            async def _send():
+                await bot.send_message(chat_id=chat_id, text=message)
+
+            asyncio.run(_send())
         except Exception as e:
             bugsnag.notify(e)
