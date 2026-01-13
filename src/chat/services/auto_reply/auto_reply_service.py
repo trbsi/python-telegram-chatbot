f06bfab3fed770807993df8c58076e3884ba6b0f
trbsi <timmy.dario@gmail.com>
2026-01-13 13:40:25 +0100

    Implement random sentence replies in AutoReplyService
    Enhance auto-reply functionality by sending random sentences.

--- src/chat/services/auto_reply/auto_reply_service.py
+++ src/chat/services/auto_reply/auto_reply_service.py
@@ -1,8 +1,10 @@
 import asyncio
+import random
+import time
 
 import bugsnag
 from telegram import Bot
-
+import re
 from chatapp import settings
 
 
@@ -11,9 +13,33 @@ class AutoReplyService:
         try:
             bot = Bot(token=settings.TELEGRAM_BOT_TOKEN)
 
+            sentence = 'I want you so bad. mmm this is Hot. I like it, do you?'
+            sentences = self._split_sentences(sentence)
+            number_of_sentences = random.randint(1, 2)
+
             async def _send():
-                await bot.send_message(chat_id=chat_id, text=message)
+                for i in range(number_of_sentences):
+                    await asyncio.sleep(random.randint(1, 3))
+                    await bot.send_message(chat_id=chat_id, text=sentences[i])
 
             asyncio.run(_send())
         except Exception as e:
             bugsnag.notify(e)
+
+    def _split_sentences(self, sentence: str) -> list:
+        sentences = re.split(r'(?<=[.!?])\s+', sentence)
+        protected_word = {'i'}
+
+        for index, sentence in enumerate(sentences):
+            if sentence.endswith('.'):
+                sentence = sentence.removesuffix('.')
+
+            sentence_split = sentence.lower().split()
+            for i, word in enumerate(sentence_split):
+                if word in protected_word:
+                    sentence_split[i] = word.capitalize()
+
+            sentence = " ".join(sentence_split)
+            sentences[index] = sentence
+
+        return sentences
